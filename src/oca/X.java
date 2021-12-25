acc05e39df6637f69137f219d25a87984a8945ab
EmrhSGLM <emrh.sglm7168@gmail.com>
2021-12-26 00:15:30 +0300

    OCA

---
+++ src/oca/X.java
@@ -0,0 +1,18 @@
+package oca;
+
+	public class X {
+
+	    int i;
+	    static int j;
+	    public static void main(String[] args) {
+	        X x1=new X();
+	        X x2=new X();
+	        x1.i=3;
+	        x1.j=4;
+	        x2.i=5;
+	        x2.j=6;
+	        System.out.println((x1.i + " " + x1.j + " " + x2.i + " " + x2.j)); // 3 6 5 6
+
+	    }
+
+}
